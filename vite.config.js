58b19d74167e84ea30b28037ad9c30dd51eb2c44
Bruno Harari <jonathansgonzalez22@gmail.com>
2025-11-18 20:09:08 -0300

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ⚙️ reemplazá "react-ecommerce" por el nombre de tu repo
-export default defineConfig({
-  plugins: [react()],
-  base: '/react-ecommerce/',
-})
-
